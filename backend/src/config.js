96f693b1d0b159627c9960be2e0062b1d935a5aa
Memoria-ORIA <ctareen@yandex.com>
2022-07-06 22:20:10 +0800

    t2

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0x47D75F58050d7ad468B854BE9c6ce74e5A09d4F4"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x9Fcc9FB84E1848E5E4a67cd9F18753687717a5c5"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
